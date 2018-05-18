480e51628fd2a81ac2a160804e3333b2e4ca990b
abller98 <abdi19771@hotmail.com>
2018-05-18 16:38:58 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,184 +1,193 @@
-//random numbre genertor
-function randomNumGenerator() {
-  let randNum;
-  let randNumGenerator = '';
-  let numbersLetters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
-  for (let i = 0; i < 6; i++) {
-    randNum = Math.floor(Math.random()*numbersLetters.length);
-    randNumGenerator += numbersLetters[randNum];
+const classNames = {
+    DELETE: "delete"
+  }
+
+  const logger = {
+    logging: false,
+    log(msg) {
+      if(this.logging) console.log(msg);
     }
-    return randNumGenerator;
   }
 
-//time and date function
-const displayTimeDate = function () {
-  let now = new Date();
-  let year = now.getFullYear();
-  let month = now.getMonth();
-  let date = now.getDate();
-  let hour = now.getHours();
-  let minutes = now.getMinutes();
-
-  return date + '.' + month + '.' + year + " " + hour + ":" + minutes;
-}
-
-//empty array
-const inventoryName = [];
-let table = document.querySelector('#inventory');
-
-function onAddButton() {
-
-  //get input values
-  let Id = document.querySelector('#random-number');
-  let itemName = document.querySelector('#item-name');
-  let itemQuantity = document.querySelector('#item-quantity');
-  let itemPrice = document.querySelector('#item-price');
-  let timeStamp = displayTimeDate();
-  
- // form validation
-  if (!itemName.value || !itemQuantity.value || !itemPrice.value) {
-    alert('Please, Fill all the boxes');
-    return false;
-  } 
-
-  //empty row is creted
-  let newRow = table.insertRow();
-
-  //creating cell
-  let idCell = newRow.insertCell(0);
-  let itemCell = newRow.insertCell(1);
-  let quanityCell = newRow.insertCell(2);
-  let priceCell = newRow.insertCell(3);
-  let timeCell = newRow.insertCell(4);
-  let actionCell = newRow.insertCell(5);
-
-  itemCell.className = 'item-cell';
-  quanityCell.className = 'quantity-cell';
-  priceCell.className = 'price-cell';
-
-
-
-    //pushing value to inventoryName arrya
-    inventoryName.push({
-          id: randomNumGenerator(),
-          name : itemName.value,
-           quantity: itemQuantity.value,
-          price: itemPrice.value,
-          time : displayTimeDate()
-    });
-    
-    //showing values in the table
-    inventoryName.forEach((item,i)=>{
-      idCell.textContent = item.id;
-      itemCell.textContent = item.name;
-      quanityCell.textContent = item.quantity;
-      priceCell.textContent = item.price;
-      timeCell.textContent = item.time;
-      actionCell.innerHTML = '<span title="Edit" class="ion-edit" onclick="onEdit(this)"></span><span title="Delete" class = "ion-trash-a" id="onDelete" onclick="removingRow(this)" ></span><span title="Completed" class="ion-close" id="toggle" onclick="toggle(this)"></span></br>';
-    })
-
-
-  //Empties the field after you add something
-  itemName.value = '';
-  itemQuantity.value = '';
-  itemPrice.value = '';
- 
-}
-
-
-const editInventory = (position, name, quantity, price) => {  //to edit name quantity and price in the array
-
-  inventoryName[position].name = name;
-  inventoryName[position].quantity = quantity;
-  inventoryName[position].price = price;
-
-};
-
-
-function removingRow(target){
-  var i = target.parentNode.parentNode.rowIndex;   //i gives the row inde
-  inventoryName.splice(i - 1, 1);   //removes from array
-  table.deleteRow(i);
-}
-
-//editing function
-
-function onEdit(edit) {
-  let editables = edit.parentNode.parentNode.childNodes;
-  let position, name, quantity, price;
-  position = parseInt(edit.parentNode.parentNode.rowIndex) - 1;
-
-  if (!editables[1].isContentEditable) {
-    editables[1].contentEditable = 'true';
-    editables[2].contentEditable = 'true';
-    editables[3].contentEditable = 'true';
-   
-    edit.style.color = '#0F0';
-    edit.title = "Update";
-
-    editables[1].style.color = 'black';
-    editables[2].style.color = 'black';
-    editables[3].style.color = 'black';
-
-    editables[1].style.backgroundColor = '#aed5e8';
-    editables[2].style.backgroundColor = '#aed5e8';
-    editables[3].style.backgroundColor = '#aed5e8';
-    
-  } else {
-   
-    name = editables[1].textContent;
-    quantity = editables[2].textContent;
-    price = editables[3].textContent;
-    editInventory(position,name,quantity,price);
-    editables[1].contentEditable = 'false';
-    editables[2].contentEditable = 'false';
-    editables[3].contentEditable = 'false';
-    
-    edit.style.color = '#000000';
-    edit.title = 'Edit',
-    
-
-    editables[1].style.backgroundColor = '#EBEDF6';
-    editables[2].style.backgroundColor = '#EBEDF6';
-    editables[3].style.backgroundColor = '#EBEDF6';
-    
-    editables[1].style.color = 'black';
-    editables[2].style.color = 'black';
-    editables[3].style.color = 'black';
-    
+  const itemProto = {
+    bought: false,
+    toggle() {
+      this.bought = !this.bought;
+      this.trigger("toggled", this)
+    }
   }
-  
-}
 
+  const items = {
+    list: [],
 
+    add(item) {
+      let newItem = Object.create(itemProto);
+      Object.assign(newItem, item, Backbone.Events);
+      newItem.on("toggled", function(item) {
+        logger.log("toggled");
+        view.addToList(item);
+      });
+      newItem.id = _.uniqueId();
+      this.list.push(newItem);
+      this.trigger("itemAdded", newItem);
+      this.trigger("updated");
+    },
 
+    delete(id) {
+      logger.log("delete: " + id);
+      let item = _.find(items.list, {
+        "id": id
+      });
 
-//to check if they are toggled
-function toggle(target) {
-    let totalCompleted = 0;
-    let inventoryLength = inventoryName.length;
-    inventoryName.forEach((item) => {
-      if (item.completed === true) {
-        totalCompleted++;
+      view.remove(item.$el);
+
+      this.list = _.pull(this.list, item);
+      this.trigger("updated");
+    },
+
+    toggle(id) {
+      _.find(items.list, {
+        "id": id
+      }).toggle();
+
+      this.trigger("updated");
+    }
+  }
+
+  const app = {
+    init() {
+      view.init();
+      Object.assign(items, Backbone.Events);
+      items.on("itemAdded", function(item) {
+        logger.log("item added");
+        view.addToList(item);
+      });
+
+      items.on("updated", function() {
+        logger.log("updated");
+        view.updateQuantities();
+      });
+    }
+  }
+
+  const view = {
+    init() {
+
+      this.$shoppingList = $("#shopping-list");
+      this.$boughtList = $("#bought-list");
+      this.$form = $("form");
+
+      const handleSubmit = function(e) {
+        e.preventDefault();
+
+        let name = $("#item-input"),
+          quantity = $("#quantity-input");
+
+        if (name.val()) {
+          items.add({
+            name: name.val(),
+            quantity: quantity.val() || 1
+          });
+        }
+
+        name.val("");
+        quantity.val("");
+
       }
-    });
-    
-    for (let i = 0; i < inventoryLength; i++) {
-      if (totalCompleted === inventoryLength) {
-        inventoryName[i].completed = false;
-        target.title = 'Completed';
-       target.className = 'ion-close';
-       target.color = '#000';
-        
-      } else {
-        for (let i = 0; i < inventoryLength; i++) {
-          inventoryName[i].completed = true;
-          target.title = 'Uncomplete';
-         target.className = 'ion-ios-checkmark';
-         target.style.color = '#384E72';
+
+      const handleClick = function(e) {
+        e.preventDefault();
+        logger.log("Clicked");
+
+        if (e.target.nodeName === "LI") {
+          let id = $(e.target).data("id").toString();
+          items.toggle(id);
+        } else if (e.target.className === classNames.DELETE) {
+          let id = $(e.target).parent().data("id").toString();
+          items.delete(id);
         }
       }
 
+      const handleDelete = function(e) {
+        e.preventDefault();
+        logger.log("Delete: " + item);
+        let id = $(e.target).data("id").toString(),
+          item = _.find(items.list, {
+            "id": id
+          });
+
+      }
+
+      $("#lists").on("click", handleClick);
+      this.$form.on("submit", handleSubmit);
+      $("." + classNames.DELETE).on("click", handleDelete);
+
+    },
+
+    addToList(item, list) {
+      let $item = item.$el || this.createListItem(item);
+
+      if (item.bought) {
+        $item.prependTo(this.$boughtList);
+      } else {
+        $item.appendTo(this.$shoppingList);
+      }
+    },
+
+    updateQuantities() {
+      logger.log("updateQuantities");
+      $("#shopping-num").html(this.$shoppingList.children().length);
+      $("#bought-num").html(this.$boughtList.children().length);
+    },
+
+    remove($el) {
+      $el.remove()
+    },
+
+    createListItem(item) {
+      item.$el = $(`<li data-id=${item.id}>${item.name} <span class="quantity">${item.quantity}</span><span class="delete">X</span></li>`);
+
+      return item.$el;
+    },
+
+    getListItem(id) {
+      let $el = $("li[data-id='" + id + "']");
+      return $el.length ? $el : null;
+    },
+
+    render(items) {
+      logger.log("render");
+
+      this.$shoppingList.empty();
+      this.$boughtList.empty();
+
+      items.forEach(item => {
+        let $item = $(`<li data-id=${item.id}>${item.name}<span>${item.quantity}</span></li>`);
+
+        if (item.bought) {
+          this.$boughtList.append($item);
+          $item.addClass("bought");
+        } else {
+          this.$shoppingList.append($item);
+        }
+      });
+
     }
-    
   }
+
+  app.init();
+
+  items.add({
+    name: "burger",
+    quantity: 1
+  });
+
+  items.add({
+    name: "pizza",
+    quantity: 1
+  });
+
+  items.add({
+    name: "Dark chocolate",
+    quantity: 1
+  });
